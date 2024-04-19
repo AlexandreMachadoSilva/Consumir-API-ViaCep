c200b61b8a93b464e3cba01fbf68f4ee12366451
Alexandre Machado <87400668+AlexandreMachadoSilva@users.noreply.github.com>
2024-04-19 18:19:52 -0300

    Update apicep.js

--- apicep.js
+++ apicep.js
@@ -1,17 +1,20 @@
+//Criando a função para consumir a API ViaCEP
 function consultaEndereco(){
-    let cep = document.querySelector('#cep').value;
-    
+    let cep = document.querySelector('#cep').value; //declarar variável para receber o cep digitado no campo da pagina
+
+    // condição para conferir se foram digitados apenas 8 digitos do CEP
     if(cep.length !== 8){
         alert('Cep inválido!');
         return;
     }
-    
+    // declara a variavel que recebe a API
     let url = `https://viacep.com.br/ws/${cep}/json/`;
-
+    //Consumindo a API
     fetch(url).then(function(response){
         response.json().then(mostrarEndereco);
     });
 }
+//função para exibir o resultado JSON na página
 function mostrarEndereco(dados){
     let resultado = document.querySelector('#resultado');
         if(dados.erro){
